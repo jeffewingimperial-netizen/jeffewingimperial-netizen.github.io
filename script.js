800808bc2cd32aa1eda143da1ea3d31b593fbce9
jeffewingimperial-netizen <jeffewing.imperial@gmail.com>
2026-07-08 23:43:19 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,3 @@
-//portfolio/
-//│── index.html
-//│── style.css
-//│── script.js
-//│── jeff.png          <-- Rename your uploaded photo to this
-//│── Jeff_Resume.pdf   <-- Your resume PDF
-
 
 
 // JEFF IMPERIAL PORTFOLIO SCRIPT
